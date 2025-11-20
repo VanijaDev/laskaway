172d5085b699ab822b5b05aa56cbac6fed9e4ad6
Ivan Solomichev <ivan@feature.io>
2025-11-20 17:23:34 -0600

    Commit build folder

---
+++ build/data/experience_tags.js
@@ -0,0 +1,11 @@
+export const experience_tags = [
+    "relax",
+    "thrill",
+    "adventure",
+    "food",
+    "weekend getaway",
+    "fun time",
+    "kids",
+    "for him",
+    "for her",
+];
